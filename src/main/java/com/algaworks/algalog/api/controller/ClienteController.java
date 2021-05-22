0f46841009a7d984f28cd7d8bd9e2033aee9bce6
Wesley Silva <wesley.sousa59@gmail.com>
2021-05-22 16:33:22 -0300

    feat: criado endpoit de excluir um cliente

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,14 @@ public class ClienteController {
 		cliente.setId(id);
 		cliente = clienteRepository.save(cliente);
 		return ResponseEntity.ok(cliente);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id) {
+		if (!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
