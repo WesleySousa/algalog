2492b0ef9d045e0ab6a5fb656bacceeec993cc92
Wesley Silva <wesley.sousa59@gmail.com>
2021-05-22 14:40:29 -0300

    feat: criado classe de cliente e o controller

---
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -0,0 +1,30 @@
+package com.algaworks.algalog.api.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+@RestController
+public class ClienteController {
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar() {
+		var cliente1 = new Cliente();
+		cliente1.setId(1L);
+		cliente1.setNome("João");
+		cliente1.setTelefone("(34) 99999-9999");
+		cliente1.setEmail("joaodascolves@algaworks.com");
+		
+		var cliente2 = new Cliente();
+		cliente2.setId(2L);
+		cliente2.setNome("Maria");
+		cliente2.setTelefone("(34) 89899-9999");
+		cliente2.setEmail("mariasilva@algaworks.com");
+		
+		return Arrays.asList(cliente1, cliente2);
+	}
+}
---
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -0,0 +1,11 @@
+package com.algaworks.algalog.domain.model;
+
+import lombok.Data;
+
+@Data
+public class Cliente {
+	private Long id;
+	private String nome;
+	private String telefone;
+	private String email;
+}
