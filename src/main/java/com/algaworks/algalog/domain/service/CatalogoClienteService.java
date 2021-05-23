e7af5ad6a7fad7ea56f69b37d5a33f2028af02ed
Wesley Silva <wesley.sousa59@gmail.com>
2021-05-23 10:57:06 -0300

    feat: criado controller para solicitar entrega

--- src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
+++ src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
@@ -15,6 +15,10 @@ public class CatalogoClienteService {
 
 	private ClienteRepository clienteRepository;
 
+	public Cliente buscar(Long id) {
+		return clienteRepository.findById(id).orElseThrow(() -> new NegocioException("Cliente não encontrado"));
+	}
+
 	@Transactional
 	public Cliente salvar(Cliente cliente) {
 		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail()).stream()
